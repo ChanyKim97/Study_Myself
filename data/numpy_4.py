65765b99895dc76aba566c7eab3d6d47369778ef
ChanyKim97 <chanhwui2@gmail.com>
2021-09-14 22:04:17 +0900

    Numpy

---
+++ data/numpy_4.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array1 = np.array([2,3,5,7,11,13,17,19,21])
+
+print(array1.max()) # 최댓값
+print(array1.min()) # 최솟값
+print(array1.mean()) # 평균값
+print(np.median(array1)) # 중앙값
+print(array1.std()) # 표준 편차
+print(array1.var()) # 분산
